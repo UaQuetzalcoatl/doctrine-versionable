b6e3bbf7b6401221ecc12eb1711c71c267cea98b
«Alex» <ua.quetzalcoatl@gmail.com>
2013-01-02 14:35:42 +0200

    Adding new folders

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+// Include Composer autoloader
+if  (
+    !($loader = @include __DIR__ . '/../vendor/autoload.php')
+    && !($loader = @include __DIR__ . '/../../../autoload.php')
+) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
+
+/* @var $loader \Composer\Autoload\ClassLoader */
+$loader->add('DoctrineVersionableTest\\', __DIR__);
