2ff205b9da618c461efe28b798d555d9ff1dd148
«Alex» <ua.quetzalcoatl@gmail.com>
2013-01-02 16:08:44 +0200

    Entities classes

---
+++ src/UaQuetzalcoatl/DoctrineVersionable/Entity/RelationVersion.php
@@ -0,0 +1,15 @@
+<?php
+namespace UaQuetzalcoatl\DoctrineVersionable\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="_relation_version")
+ *
+ * @author alex
+ */
+class RelationVersion
+{
+    //put your code here
+}
